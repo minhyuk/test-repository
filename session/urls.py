af2d41235636532d9d8ec746e521842f4399d830
killerwhalee <killerwhalee@kaist.ac.kr>
2024-01-21 22:36:10 +0900

    Added full Respiratory Graph url routes

--- session/urls.py
+++ session/urls.py
@@ -6,8 +6,12 @@ app_name = 'session'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # Respiratory Graph
     path('rg/', views.rg_record, name='rg-record'),
     path('rg/<str:username>', views.rg_inquiry, name='rg-inquiry'),
-    # path('<str:username>', views.medass_inquiry, name='inquiry'),
-    # path('delete/<int:id>', views.medass_delete, name='delete'),
+    path('rg/delete/<int:id>', views.rg_delete, name='rg-delete'),
+    # Sustained Attention
+    # path('sa/', views.rg_record, name='sa-record'),
+    # path('sa/<str:username>', views.rg_inquiry, name='sa-inquiry'),
+    # path('sa/delete/<int:id>', views.rg_delete, name='sa-delete'),
 ]
